6f12e334f932ee8d521a76e98de5b62a056322c2
Kamrul <khannahid361@gmail.com>
2024-08-20 17:38:51 +0600

    sssd

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact Us</router-link>
   </nav>
   <router-view/>
 </template>
@@ -22,9 +23,13 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  padding: 10px;
+  border-radius: 4px;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background-color: crimson;
 }
 </style>
---
+++ src/views/ContactusView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contactus">
+      <h1>This is an Contact Us page</h1>
+    </div>
+  </template>
+  
+
+<script>
+    export default {
+        name : "ContactusView",
+    }
+</script>
+
+<style scoped>
+
+</style>
